37d51cf4748f6216b91afb00f209168778caf2b0
KexuanZhang <kex.zhang@mail.utoronto.ca>
2021-11-13 15:33:49 -0500

    Post!!

--- phase1/Post/Post.java
+++ phase1/Post/Post.java
@@ -5,23 +5,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Post {
+    private String title;
     private String text;
-    private ArrayList<Comment> comments;
+    private ArrayList<Post> comments; //a sub post can only be a comment.
     private int likes;
     private User postmaker;
 
-    public Post(String text, User postmaker){
+    public Post(String text, User postmaker, ArrayList photos, String title){//Constructor for a post
+        this.title = title;
         this.text = text;
         this.comments = new ArrayList<>();
         this.likes = 0;
         this.postmaker = postmaker;
     }
 
+    public Post(String text, User postmaker, ArrayList photos){//Constructor for a comment
+        this.title = null;
+        this.text = text;
+        this.comments = new ArrayList<>();
+        this.likes = 0;
+        this.postmaker = postmaker;
+    }
+
+    public Post getPost(PostManager manager, int postIndex){
+        return manager.getPostList().get(postIndex);
+    }
+
     public String getText(){
         return this.getText();
     }
 
-    public void addComment(Comment comment) {
+    public void editText(String newText) { this.text = newText;}
+
+    public void addComment(Post comment) {
         this.comments.add(comment);
     }
 
--- phase1/Post/PostManager.java
+++ phase1/Post/PostManager.java
@@ -6,31 +6,39 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PostManager {
-    private ArrayList postlist;
+    private ArrayList<Post> postlist;
 
     public PostManager(){
         this.postlist = new ArrayList();
     }
 
-    public void makeMainPost(String title, User postmaker, String text, ArrayList<String> photos){
-        MainPost mainPost = new MainPost(title, text, postmaker, photos);
-        this.postlist.add(mainPost);
+    public ArrayList<Post> getPostList(){
+        return this.postlist;
     }
 
-    public void makeComment(String text, User postmaker, Post post){
-        Comment comment = new Comment(text, postmaker);
+    public void makePost(String text, User postmaker, ArrayList photos, String title){
+        Post post = new Post(text, postmaker, photos, title);
+        this.postlist.add(post);
+    }
+
+    public void makeComment(String text, User postmaker, ArrayList photos, Post post){
+        Post comment = new Post(text, postmaker, photos);
         post.addComment(comment);
     }
 
-    public void deleteMainPost(MainPost current_post){
+    public void deleteMainPost(Post current_post){
         this.postlist.remove(current_post);
     }
 
-    public void deleteComment(Post current_post, Comment current_comment){
+    public void deleteComment(Post current_post, Post current_comment){
         current_post.removeComment(current_comment);
     }
 
     public void likePost(Post post){
         post.addLike();
+    }
+
+    public void editPost(Post post, String newText) {
+        post.editText(newText);
     }
 }
